2233d7b35154ee792a169ec38c8fc78e72d437f9
unknown <deepudeepthi283@gmail.com>
2020-11-16 19:08:14 +0530

    main file added

---
+++ main.py
@@ -0,0 +1,7 @@
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+number = int(input())
+num = phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(num,"en"))
+print(geocoder.description_for_number(num,"en"))
